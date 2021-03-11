2b5398e9d4164d1a6352f3ed78c61389a0ce4569
rodryggodev <rodryggolacort@hotmail.com>
2021-03-10 23:48:11 -0300

    condições parte1

---
+++ condicoes.js
@@ -0,0 +1,21 @@
+// CONDIÇÕES PT1 
+
+// CONDIÇÃO SIMPLES SÓ COM O IF(SE)
+
+
+let nota = 7;
+
+if (nota >= 7.7){
+    console.log('aprovado');
+}
+
+
+// CONDIÇÃO COMPOSTA IF E ELSE
+
+let pais = 'BR';
+
+if(pais !='BR'){
+    console.log('gringo safado')
+} else{
+    console.log('br ladrão mais safado ainda')
+}
